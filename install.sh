b8121160bf133eb91406e77f540f48044370bfcc
Matthias Nunes <matthiasnunes@gmail.com>
2014-12-17 03:14:17 -0200

    Install script working... at least the basic stuff to install

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-[ -d ~/.vim/bundle/Vundle.vim ] || git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+rm -rf $(pwd)/bundle/Vundle.vim/
+#Works like an if
+#[ -d ~/.vim/bundle/Vundle.vim ] || git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -sf $(pwd)/vimrc ~/.vimrc
 vim +PluginInstall +PluginClean +qall
-cd ~/.vim/vimproc.vim
+cd ~/.vim/bundle/vimproc.vim
 make
