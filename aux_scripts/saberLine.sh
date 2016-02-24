e90b3cd42cf8d1708a8483154370da3bffef1721
Matthias Nunes <execb5@users.noreply.github.com>
2016-02-24 20:34:35 -0300

    adds saberLine.sh
    a useless script that prints saber line in the beginning of fate/stay night

---
+++ aux_scripts/saberLine.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+echo "「問おう。あなたが、私のマスターか」"
+echo "「サーヴァント・セイバー、召喚に従い参上した。これより我が剣は汝と共にあり、あなたの運命は私と共にある―――"
+echo " ここに、契約は完了した」"
+echo ""
+echo "Anata ga, watashi no masutā ka"
+echo "sāvanto seibā, shōkan ni shitagai sanjō shita. Kore yori waga ken wa nanji to tomoni ari, anata no unmei wa watashi to tomoni aru ―――"
+echo "koko ni, keiyaku wa kanryō shita"
