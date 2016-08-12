d5b9c23364320b964a49335c03dfc114a5f2952b
Matthias Nunes <matthiasnunes@gmail.com>
2016-08-12 02:28:19 -0300

    blabla

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ln -sf $(pwd)/vimrc ~/.vimrc
 echo "vimrc symlinked"
