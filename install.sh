2d4fdf959b090eef3459f25b379d5e6959be4252
Matthias Nunes <matthiasnunes@gmail.com>
2019-11-21 10:41:05 -0300

    Fixes shellcheck issues

--- install.sh
+++ install.sh
@@ -7,29 +7,29 @@ set -o nounset
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-ln -sf $(pwd)/vimrc ~/.vimrc
+ln -sf "$(pwd)/vimrc" ~/.vimrc
 echo "vimrc symlinked"
-ln -sf $(pwd)/gitconfig ~/.gitconfig
+ln -sf "$(pwd)/gitconfig" ~/.gitconfig
 echo "gitconfig symlinked"
-ln -sf $(pwd)/tmux.conf ~/.tmux.conf
+ln -sf "$(pwd)/tmux.conf" ~/.tmux.conf
 echo "tmux.conf symlinked"
-ln -sf $(pwd)/latexmkrc ~/.latexmkrc
+ln -sf "$(pwd)/latexmkrc" ~/.latexmkrc
 echo "latexmkrc symlinked"
-ln -sf $(pwd)/gitignore ~/.gitignore
+ln -sf "$(pwd)/gitignore" ~/.gitignore
 echo ".gitignore symlinked"
 mkdir -p ~/.ssh
-ln -sf $(pwd)/ssh_config ~/.ssh/config
+ln -sf "$(pwd)/ssh_config" ~/.ssh/config
 echo ".ssh_config symlinked"
-ln -sf $(pwd)/Xresources ~/.Xresources
+ln -sf "$(pwd)/Xresources" ~/.Xresources
 echo "Xresources symlinked"
 xrdb -merge ~/.Xresources
 echo "Xresources merged"
 
 [ -d ~/.ctags.d ] || mkdir ~/.ctags.d
-for f in $(pwd)/ctags/*.ctags
+for f in "$(pwd)"/ctags/*.ctags
 do
 	tag_file=${f##*/}
-	ln -sf $f ~/.ctags.d/$tag_file
+	ln -sf "$f" ~/.ctags.d/"$tag_file"
 	echo "$tag_file symlinked"
 done
 
@@ -42,7 +42,7 @@ echo "Setting oh my zsh..."
 [ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ] || git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 echo "oh my zsh configured!"
 
-ln -sf $(pwd)/zshrc ~/.zshrc
+ln -sf "$(pwd)/zshrc" ~/.zshrc
 echo "zshrc symlinked"
 
 grep copythis zshrc >> ~/.bashrc
@@ -50,14 +50,14 @@ echo ".bashrc configuration created/appended"
 
 
 [ -d ~/.mpd ] || mkdir ~/.mpd
-ln -sf $(pwd)/mpd.conf ~/.mpd/mpd.conf
+ln -sf "$(pwd)/mpd.conf" ~/.mpd/mpd.conf
 touch ~/.mpd/mpd.db
 touch ~/.mpd/mpd.log
 touch ~/.mpd/mpdstate
 mkdir -p ~/.mpd/playlists
 
 [ -d ~/.ncmpcpp ] || mkdir ~/.ncmpcpp
-ln -sf $(pwd)/ncmpcpp ~/.ncmpcpp/config
+ln -sf "$(pwd)/ncmpcpp" ~/.ncmpcpp/config
 
 mkdir -p ~/.vimtmp
 echo ".vimtmp directory set up"
